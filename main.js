ec341dca3754bb6d49d1551bdd1df828fbe825c1
Tetiana Kurdyukova <tetiana@kurdyukov.info>
2019-01-26 21:34:26 +0200

    added random squares, random speed and random color

--- main.js
+++ main.js
@@ -1,18 +1,46 @@
 var squares = new Array();
-squares.push({
-    size: 20,
-    color: '#f0f',
-    speed: 1,
-    x: 320,
-    y: 0
-});
 
-var currentPos = 0;
+const CREATE_FREQ = 1500; // 1.5 sec
+const MAX_SPEED = 4; // 4px
+const SQUARE_WIDTH = 20;
+var createOn = now() + getRandomInt(1000, CREATE_FREQ);
+
+function now() {
+    return new Date().getTime();
+}
+
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+}
+
+function createNewSquare(canvas) {
+    return {
+        size: SQUARE_WIDTH,
+        color: getRandomColor(),
+        speed: getRandomInt(1, MAX_SPEED),
+        x: getRandomInt(0, canvas.clientWidth - SQUARE_WIDTH),
+        y: 0
+    };
+}
 
 function recalc() {
     var canvas = document.getElementById('canvas');
+
     // create
-    
+    if (now() > createOn) {
+        squares.push(createNewSquare(canvas));
+        createOn = now() + getRandomInt(1000, CREATE_FREQ);
+    }
+
     // move
     squares.forEach(function(s) {
         s.y += s.speed;
@@ -21,6 +49,7 @@ function recalc() {
     squares = squares.filter(function(s) {
         return s.y < canvas.clientHeight;
     });
+
 }
 
 function render() {
@@ -28,10 +57,10 @@ function render() {
     var ctx = canvas.getContext('2d');  
     ctx.clearRect(0, 0, canvas.clientWidth, canvas.clientWidth);
     
-    squares.forEach( function(s) {
+    squares.forEach(function(s) {
+        ctx.fillStyle = s.color;
         // x, y, size_left, size_top
         ctx.fillRect(s.x, s.y, s.size, s.size);
-        ctx.fillStyle = s.color;
     });
 }
 
